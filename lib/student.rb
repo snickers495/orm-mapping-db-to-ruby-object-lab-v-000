3d1eec188862fe5f592991a34783a1b81a057370
Lisa Lee <snickers495@gmail.com>
2017-10-20 21:41:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,4 +77,13 @@ class Student
       self.new_from_db(row)
     end
   end 
+
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+          SELECT * FROM students WHERE grade = 10
+          SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end 
 end
