a4321d73fbae33c4f1293f5a823c1ca090d26697
Lisa Lee <snickers495@gmail.com>
2017-10-20 21:40:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,11 +61,11 @@ class Student
   end
 
   def self.count_all_students_in_grade_9
-    sql = <<-SQL 
+    sql = <<-SQL
           SELECT * FROM students WHERE grade = 9
           SQL
-    DB[:conn].execute(sql).map do |row| 
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end 
+    end
   end 
 end
